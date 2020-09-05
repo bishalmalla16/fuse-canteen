f060d39cdd4b09dfcf371f07e498767de642ed38
Bishal Malla <bishalmalla16@gmail.com>
2020-09-05 18:56:12 +0545

    getting started with fuse canteen

---
+++ src/main/java/com/fusemachine/FuseCanteenApplication.java
@@ -0,0 +1,13 @@
+package com.fusemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuseCanteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuseCanteenApplication.class, args);
+	}
+
+}
