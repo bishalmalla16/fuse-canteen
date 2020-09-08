b5bc43d2ed4fec36b031d4f2f38660d501c255bc
Bishal Malla <bishalmalla16@gmail.com>
2020-09-09 00:37:25 +0545

    implemented bcrypt password encoder

--- src/main/java/com/fusemachine/service/UserService.java
+++ src/main/java/com/fusemachine/service/UserService.java
@@ -3,6 +3,7 @@ package com.fusemachine.service;
 import com.fusemachine.entity.User;
 import com.fusemachine.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +13,10 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     public List<User> findAll(){
         return userRepo.findAll();
@@ -23,6 +28,7 @@ public class UserService {
 
     @Transactional
     public void save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
     }
 
