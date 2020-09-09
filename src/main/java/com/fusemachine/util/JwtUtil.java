4d6be355ba7fbd6bc0616b03eefe0e5c4dd2c894
Bishal Malla <bishalmalla16@gmail.com>
2020-09-09 12:40:52 +0545

    updated token creation

--- src/main/java/com/fusemachine/util/JwtUtil.java
+++ src/main/java/com/fusemachine/util/JwtUtil.java
@@ -45,7 +45,7 @@ public class JwtUtil {
     private String createToken(Map<String, Object> claims, String username) {
         return Jwts.builder().setClaims(claims).setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*5))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
